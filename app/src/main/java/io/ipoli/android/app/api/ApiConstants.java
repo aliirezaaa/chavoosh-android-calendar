d20d5a06cd2ce6ddeecd458dcb302da03ebfcb32
Alireza Javaheri <aliirezaaa@gmail.com>
2017-05-14 12:14:53 +0430

    add all files to git

---
+++ app/src/main/java/io/ipoli/android/app/api/ApiConstants.java
@@ -0,0 +1,13 @@
+package io.ipoli.android.app.api;
+
+public interface ApiConstants {
+    String DEV_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
+    String PROD_IPOLI_ENDPOINT = "http://10.0.3.2/v1/";
+    String DEV_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
+    String PROD_SCHEDULING_ENDPOINT = "http://10.0.3.2/v1/";
+    String DEV_API_URL= "http://10.0.3.2/v1/";
+    String DEV_SYNC_URL= "http://10.0.3.2/v1/";
+    String PROD_SYNC_URL= "http://10.0.3.2/v1/";
+    String PROD_API_URL= "http://10.0.3.2/v1/";
+    String API_KEY = "test";
+}
---
+++ app/src/main/java/io/ipoli/android/app/locals/AnalyticsConstants.java
@@ -0,0 +1,10 @@
+package io.ipoli.android.app.locals;
+
+/**
+ * Created by Alireza on 5/6/2017.
+ */
+
+public interface AnalyticsConstants {
+    String PROD_FLURRY_KEY = "123456";
+    String DEV_FLURRY_KEY = "42";
+}
